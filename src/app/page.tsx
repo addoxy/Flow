80eb1cf5d924a52218c1134a30b37192df6cf119
Aditya Kumar <adityakumar13043@gmail.com>
2024-10-07 15:02:43 +0000

    reorder cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div className="grid grid-cols-1 gap-4 p-4 sm:gap-6 sm:p-6 lg:grid-cols-2">
       <Timer />
-      <TaskManager />
       <SongPlayer />
+      <TaskManager />
       <Notes />
     </div>
   );
